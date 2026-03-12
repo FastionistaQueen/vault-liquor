06d7d2c377e848eca676470bf84afed2b9ac7190
FastionistaQueen <142240568+FastionistaQueen@users.noreply.github.com>
2026-03-12 11:45:33 +0300

    Create Product.js Mongoose schema for liquor product

---
+++ server/models/Product.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    imageURL: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+// Update updatedAt field before saving document
+productSchema.pre('save', function(next) {
+    this.updatedAt = Date.now();
+    next();
+});
+
+module.exports = mongoose.model('Product', productSchema);
